ce828081e05de068d7cf1173788b9386ff8f01d8
theGeekkit <sonofgondolin16@gmail.com>
2023-08-29 16:14:54 -0500

    8-29-23
    Co-authored-by: Tom <trankin@users.noreply.github.com>

--- testItems.js
+++ testItems.js
@@ -1,9 +1,5 @@
 const fs = require("fs");
-
-const currentDate = new Date();
-
-console.log(currentDate);
-
+const { features } = require("process");
 
 async function findReferencedIds(obj) {
   const referencedIds = [];
@@ -22,27 +18,30 @@ async function findReferencedIds(obj) {
 async function run() {
   const dat = fs.readFileSync("first_update.json");
   const obj = JSON.parse(dat);
+  const currentDate = new Date();
+
+  console.log(currentDate);
 
   const referencedIds = await findReferencedIds(obj);
 
   console.log(referencedIds);
   const notReferencedFeatures = [];
   obj.features.forEach((feature) => {
-    if (!referencedIds.includes(feature.id) && feature.properties.messageType !== 'Cancel') {
-
-
-        if(!referencedIds.includes(features.properties.expiration)) {
-            notReferencedFeatures.push(feature)
-        }
-    
-    
-    
-    
-    
-    }    
+    const expirationDate = new Date(feature.properties.expires);
+    // console.log(expirationDate, currentDate);
+    if (
+      !referencedIds.includes(feature.id) &&
+      feature.properties.messageType !== "Cancel" &&
+      expirationDate > currentDate
+    ) {
+      notReferencedFeatures.push(feature);
+    }
   });
-  console.log(notReferencedFeatures)
-  fs.writeFileSync("notReferencedFeatures.json", JSON.stringify(notReferencedFeatures))
+  console.log(notReferencedFeatures);
+  fs.writeFileSync(
+    "notReferencedFeatures.json",
+    JSON.stringify(notReferencedFeatures)
+  );
 }
 
 run().catch((error) => {
