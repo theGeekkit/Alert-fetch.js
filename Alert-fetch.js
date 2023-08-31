5294b28b9922e9b82dd2537a74ba0bdffecd559c
theGeekkit <sonofgondolin16@gmail.com>
2023-08-30 19:35:23 -0500

    formatted

--- Alert-fetch.js
+++ Alert-fetch.js
@@ -1,55 +1,45 @@
-const axios = require('axios');
-const fs = require('fs');
+const axios = require("axios");
+const fs = require("fs");
 
 async function run() {
-    // setInterval(async () => {
-    //     await fetchData();
-    // }, 1000);
-    await fetchData();
+  // setInterval(async () => {
+  //     await fetchData();
+  // }, 1000);
+  await fetchData();
 }
 
 async function fetchData() {
+  // Make a GET request
+  try {
+    let response = await axios.get(
+      "https://api.weather.gov/alerts?point=37.01161240210997,-89.60530401388498"
+    );
 
-    // Make a GET request
-    try {
-        let response = await axios.get('https://api.weather.gov/alerts?point=37.01161240210997,-89.60530401388498');
+    let obj = response.data;
+    let features = obj.features;
 
-        let obj = response.data;
-        let features = obj.features;
+    //features.properties.references
+    let topLevelFeatures = features.filter(
+      (obj) => obj.properties.references.length <= 5
+    );
 
-        //features.properties.references
-        let topLevelFeatures = features.filter(obj => obj.properties.references.length <= 5);
+    obj.features = topLevelFeatures;
 
+    fs.writeFileSync("fourth_update.json", JSON.stringify(obj));
 
-        obj.features = topLevelFeatures;
+    console.log(topLevelFeatures.length);
 
-        fs.writeFileSync('fourth_update.json', JSON.stringify(obj));
+    return;
 
-        
+    console.log(response.data);
+    let data = response.data;
+    console.log(data.features.length);
+    data.features.forEach((feature) => {
+      console.log(feature.id);
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
 
-
-        console.log(topLevelFeatures.length);
-
-        return;
-
-
-        console.log(response.data);
-         let data = response.data;
-         console.log(data.features.length);
-         data.features.forEach(feature => {
-             console.log(feature.id);
-         });
-    } catch (error) {
-        console.error('Error:', error);
-
-
-
+  run().then(() => console.log("DONE"));
 }
-
-
-
-
-
-
-
-run().then(() => console.log("DONE"))};
