78feea4bd7991125b35bfd0da368f943997f574a
theGeekkit <sonofgondolin16@gmail.com>
2023-08-30 19:33:47 -0500

    8-30-23

--- testItems.js
+++ testItems.js
@@ -1,70 +1,12 @@
-// const fs = require("fs");
-// const { features } = require("process");
-// let urgentImmediate = {};
-// async function findReferencedIds(obj) {
-//   const referencedIds = [];
-//   urgentImmediate = obj.features.filter(
-//     (feature) => feature.urgency === "Immediate"
-//   );
-//   console.log(urgentImmediate);
-//   obj.features.forEach((feature) => {
-//     feature.properties.references.forEach((reference) => {
-//       if (!referencedIds.includes(reference["@id"])) {
-//         referencedIds.push(reference["@id"]);
-//       }
-//     });
-//   });
-
-//   return referencedIds;
-// }
-
-// async function run() {
-//   const dat = fs.readFileSync("first_update.json");
-//   const obj = JSON.parse(dat);
-//   const currentDate = new Date();
-//   console.log(currentDate);
-
-//   const referencedIds = await findReferencedIds(obj);
-
-//   console.log(referencedIds);
-//   const notReferencedFeatures = [];
-
-//   obj.features.forEach((feature) => {
-//     const expirationDate = new Date(feature.properties.expires);
-//     // console.log(expirationDate, currentDate);
-//     if (
-//       !referencedIds.includes(feature.id) &&
-//       feature.properties.status === "Actual" &&
-//       feature.properties.messageType !== "Cancel" &&
-//       expirationDate > currentDate &&
-//       urgentImmediate
-//     ) {
-//       notReferencedFeatures.push(feature);
-
-//       fs.writeFileSync(
-//         "notReferencedFeatures.json",
-//         JSON.stringify(notReferencedFeatures)
-//       );
-//     }
-//   });
-//   console.log(notReferencedFeatures);
-// }
-
-// run().catch((error) => {
-//   console.error(error);
-// });
-
-
 const fs = require("fs");
-
+const { features } = require("process");
+urgentImmediate = {};
 async function findReferencedIds(obj) {
   const referencedIds = [];
-  const urgentImmediate = obj.features.filter(
+  urgentImmediate = obj.features.filter(
     (feature) => feature.urgency === "Immediate"
   );
-
   console.log(urgentImmediate);
-
   obj.features.forEach((feature) => {
     feature.properties.references.forEach((reference) => {
       if (!referencedIds.includes(reference["@id"])) {
@@ -73,7 +15,7 @@ async function findReferencedIds(obj) {
     });
   });
 
-  return { referencedIds, urgentImmediate };
+  return referencedIds;
 }
 
 async function run() {
@@ -82,30 +24,28 @@ async function run() {
   const currentDate = new Date();
   console.log(currentDate);
 
-  const { referencedIds, urgentImmediate } = await findReferencedIds(obj);
+  const referencedIds = await findReferencedIds(obj);
 
   console.log(referencedIds);
   const notReferencedFeatures = [];
-
   obj.features.forEach((feature) => {
     const expirationDate = new Date(feature.properties.expires);
-
+    // console.log(expirationDate, currentDate);
     if (
       !referencedIds.includes(feature.id) &&
       feature.properties.status === "Actual" &&
       feature.properties.messageType !== "Cancel" &&
       expirationDate > currentDate &&
-      urgentImmediate.some((urgent) => urgent.id === feature.id) // Checking if current feature is in urgentImmediate
+      urgentImmediate
     ) {
       notReferencedFeatures.push(feature);
-      fs.writeFileSync(
-        "notReferencedFeatures.json",
-        JSON.stringify(notReferencedFeatures)
-      );
     }
   });
-
   console.log(notReferencedFeatures);
+  fs.writeFileSync(
+    "notReferencedFeatures.json",
+    JSON.stringify(notReferencedFeatures)
+  );
 }
 
 run().catch((error) => {
