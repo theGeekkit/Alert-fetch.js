97bc77da6eabb852002301d3075a0ae1f6641cab
theGeekkit <sonofgondolin16@gmail.com>
2023-09-11 11:13:34 -0500

    fixed error

--- testItems.js
+++ testItems.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 let shouldAlert = [];
-let alertedFeaturedIds = [];
+let previousAlertedIds = new Set();
 
 async function findReferencedIds(obj) {
   const referencedIds = [];
